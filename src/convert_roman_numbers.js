7fd246af17a93017a63b8138b02ae79a60331398
Jagath A <WSO2USER05@rebeccas-ipad.home>
2022-12-29 19:30:36 +0000

    added code to handle number 40

--- src/convert_roman_numbers.js
+++ src/convert_roman_numbers.js
@@ -3,6 +3,11 @@ module.exports = decToRoman;
 function decToRoman(number) {
   let romanNumeral = "";
 
+  if (number >= 40) {
+    romanNumeral += "XL";
+    number -= 40;
+  }
+
   while (number >= 10) {
     romanNumeral += "X";
     number -= 10;
