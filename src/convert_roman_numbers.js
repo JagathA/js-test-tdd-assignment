a0e9afbe4416f7cc869fc866e19fa232913d2728
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2022-12-31 09:59:39 +0000

    added code to handle error conditions

--- src/convert_roman_numbers.js
+++ src/convert_roman_numbers.js
@@ -4,6 +4,10 @@ const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 const  roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
 
 function decToRoman(number) {
+
+  if (number === undefined) throw new Error("number is required");
+  if (number === 0) throw new Error("Non zero value is required");
+
   let romanNumeral = "";
 
   for (const i in decimal) {
@@ -12,7 +16,6 @@ function decToRoman(number) {
       number -=  decimal[i];
     }
  }
-
 
   return romanNumeral;
 }
