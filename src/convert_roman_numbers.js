b27579a54e4b7fe80bc8f98e62fffc4e2fcdbfac
Jagath A <WSO2USER05@WSO2s-MacBook-Pro-2.local>
2022-12-31 09:48:21 +0000

    refactored to use a for in loop

--- src/convert_roman_numbers.js
+++ src/convert_roman_numbers.js
@@ -1,73 +1,18 @@
 module.exports = decToRoman;
 
+const decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const  roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+
 function decToRoman(number) {
   let romanNumeral = "";
 
-  while (number >= 1000) {
-    romanNumeral += "M";
-    number -= 1000;
-  }
-
-  if (number >= 900) {
-    romanNumeral += "CM";
-    number -= 900;
-  }
-
-  if (number >= 500) {
-    romanNumeral += "D";
-    number -= 500;
-  }
-
-  if (number >= 400) {
-    romanNumeral += "CD";
-    number -= 400;
-  }
-
-  while (number >= 100) {
-    romanNumeral += "C";
-    number -= 100;
-  }
-
-  if (number >= 90) {
-    romanNumeral += "XC";
-    number -= 90;
-  }
-
-  if (number >= 50) {
-    romanNumeral += "L";
-    number -= 50;
-  }
-  
-  if (number >= 40) {
-    romanNumeral += "XL";
-    number -= 40;
-  }
-
-  while (number >= 10) {
-    romanNumeral += "X";
-    number -= 10;
-  }
-
-  if (number === 9) {
-    romanNumeral += "IX";
-    number -= 9;
-  }
-
-  if (number >= 5) {
-    romanNumeral += "V";
-    number -= 5;
-  }
-
-  if (number === 4) {
-    romanNumeral += "IV";
-    number -= 4;
-  }
-
-  if (number < 4) {
-    for (let i = 0; i < number; i++) {
-      romanNumeral += "I";
+  for (const i in decimal) {
+    while (number >= decimal[i]) {
+      romanNumeral += roman[i];
+      number -=  decimal[i];
     }
-  }
+ }
+
 
   return romanNumeral;
 }
